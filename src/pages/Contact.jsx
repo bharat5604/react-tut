c7992c57c9415869863d6d4a2c4589b9d85d4eb1
Nishant <nishantsingh95340@gmail.com>
2023-03-09 02:14:43 +0530

    adding form in contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const Contact = () => {
   return (
     <div>
-      <div>this is contact page in react tut</div>
+      <div>
+        <form action="">
+          <input type="text" />
+          <button type="submit" className="bg-black text-white px-4 py-1 rounded-md">
+            onsubmit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
